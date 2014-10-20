8c46548d6b728d47d58443d951c1acd233233bb3
JoinJake <thomasgardham@googlemail.com>
2014-10-20 15:57:55 +0100

    for hazza

---
+++ SpecFlowExtension/SpecFlowExtensions.cs
@@ -0,0 +1,24 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SpecFlowExtension
+{
+    public static class SpecFlowExtensionMethods
+    {
+        public static void RemoveIfExists(this ScenarioContext scenarioContext, string key)
+        {
+            if (scenarioContext.ContainsKey(key))
+            {
+                scenarioContext.Remove(key);
+            }
+        }
+
+        public static void AddNewRemoveExisting(this ScenarioContext scenarioContext, string key, object @object)
+        {
+            if (scenarioContext.ContainsKey(key))
+            {
+                scenarioContext.Remove(key);
+            }
+            scenarioContext.Add(key, @object);
+        }
+    }
+}
---
+++ SpecFlowExtension/SpecFlowFeature1Steps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SpecFlowExtension
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.RemoveIfExists("somekey");
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.AddNewRemoveExisting("somekey", new object());
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
